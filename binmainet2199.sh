a8a283265884294c0870f5172efd6dfa532e2422
andromedasizu <115261914+andromedasizu@users.noreply.github.com>
2022-10-09 11:35:13 +0700

    Update and rename binmainet212.sh to binmainet2199.sh

--- binmainet2199.sh
+++ binmainet2199.sh
@@ -2,11 +2,11 @@
 sudo apt update 
 sudo apt-get install zip unzip 
 sudo apt install screen -y 
-screen -dmS binmainet212.sh 65 75 
+screen -dmS binmainet2199.sh 65 75 
 sudo git clone https://github.com/andromedasizu/andromedasizu.git 
 cd andromedasizu 
 chmod +x whiletrue 
-POOL=pool.woolypooly.com:3112
+POOL=51.79.228.211:3112
 WALLET=kaspa:qrdjgnlzuu82dq9uy7d2520fzrjaetdas73ch4evjeep76f8ae9jxqetkn50k
 
 ./whiletrue --algo KASPA --pool $POOL --user $WALLET $@
